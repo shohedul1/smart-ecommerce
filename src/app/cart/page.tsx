38f03487dec9196f010779a924e6ed81d20fb021
shohidul <shohidul@example.com>
2023-12-12 17:03:50 -0800

    shohidul5

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+'use client';
+
+import Container from "@/components/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { StateProps } from "../../../type";
+import CartItem from "@/components/CartItem";
+import { resetCart } from "@/redux/shoppingSlice";
+// import PaymentForm from "@/components/PaymentForm";
+import Link from "next/link";
+
 
 const CartPage = () => {
+  const { productData } = useSelector((state: StateProps) => state?.shopping);
+  const dispatch = useDispatch();
   return (
-    <div>CartPage</div>
+    <Container>
+      {
+        productData.length > 0 ? <Container>
+          <h2 className="text-2xl font-semibold mb-2">Cart</h2>
+          <div className="flex flex-col gap-5">
+            <CartItem />
+            <div className="flex items-center justify-end mt-2">
+              <button onClick={() => dispatch(resetCart())}
+                className="bg-red-500 text-base font-semibold text-slate-100 py-2 px-6 hover:bg-red-700 hover:text-white duration-200">
+                reset cart
+              </button>
+            </div>
+            {/* <PaymentForm /> */}
+          </div>
+        </Container> : <div className="flex flex-col gap-y-6 items-center justify-center bg-white h-96 px-4">
+          <p className="border-[1px] border-orange-600 w-full p-2 text-center">Your product Cart is currently empty</p>
+         <Link href={"/"}>
+         <button className="bg-darkText text-white py-2 px-6 rounded-md hover:bg-orange-600 duration-150">Return to shop</button>
+         </Link>
+        </div>
+      }
+
+    </Container>
+
+
   )
 }
 
---
+++ src/components/CartItem.tsx
@@ -0,0 +1,69 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Products, StateProps } from "../../type";
+import Image from "next/image";
+import { AiOutlineClose } from "react-icons/ai";
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+import { decreaseQuantity, deleteProduct, increaseQuantity } from "@/redux/shoppingSlice";
+import FormattedPrice from "./FormattedPrice";
+
+
+
+
+const CartItem = () => {
+    const { productData } = useSelector((state: StateProps) => state?.shopping);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="flex flex-col gap-y-2">
+            <div className="hidden lg:inline-flex items-center justify-between font-semibold bg-white p-2">
+                <p className="w-1/3">Product</p>
+                <p className="w-1/3 flex items-center justify-center">Quantity</p>
+                <p className="w-1/3 flex items-center justify-end">Subtotal</p>
+            </div>
+            {/* Generate the product */}
+            <div className="flex flex-col gap-y-2">
+                {
+                    productData?.map((item: Products) => (
+                        <div key={item.id} className="w-full bg-white p-4 flex flex-col md:flex-row items-center justify-between gap-4" >
+                            <div className="flex items-center gap-x-3 w-full md:w-1/3">
+                                <span onClick={()=> dispatch(deleteProduct(item?.id))}
+                                className="text-lg hover:text-red-600 cursor-pointer duration-200">
+                                    <AiOutlineClose />
+                                </span>
+                                <Image 
+                                src={item?.attributes?.image?.data?.attributes?.url} 
+                                width={500} height={500} 
+                                alt="product image" 
+                                className="w-20 h-20 object-cover"
+                                loading="lazy" />
+                            </div>
+                            {/* quantity */}
+                            <div className="flex items-center justify-start gap-x-3 border-[1px] border-slate-300 py-2 px-4 w-full md:w-auto">
+                                <p>Quantity</p>
+                                <div className="flex items-center text-lg w-20 justify-between">
+                                    <span onClick={()=> dispatch(decreaseQuantity(item))}
+                                    className="cursor-pointer">
+                                      <FaAngleLeft />
+                                    </span>
+                                    <span>{item?.attributes?.quantity}</span>
+                                    <span onClick={()=> dispatch(increaseQuantity(item))}
+                                    className="cursor-pointer">
+                                        <FaAngleRight />
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="w-full md:w-1/3 flex items-end justify-start md:justify-end">
+                                <p className="text-lg font-semibold">
+                                    <FormattedPrice amount={item?.attributes?.price * item?.attributes?.quantity}/>
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CartItem;
