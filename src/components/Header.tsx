55c6545d58cea3309ee87fd398f7cea94992b6a8
shohidul <shohidul@example.com>
2023-12-13 08:24:12 -0800

    shohidul12

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Container from "./Container"
 import Logo from "./Logo";
 // import { FiSearch } from "react-icons/fi";
 import { BsCart } from "react-icons/bs";
-
+import { CiUser } from "react-icons/ci";
 import { FiLogOut } from "react-icons/fi";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from 'next/image';
@@ -52,70 +52,68 @@ const Header = () => {
     }, [productData]);
 
     return (
-        <div className='bg-bodyColor h-20 top-0 sticky z-50'>
-            <Container className='h-full flex items-center gap-x-2  sm:gap-x-4  md:gap-x-5 justify-between'>
-                {/* {logo} */}
-                <Logo />
-            
-                {/* Search Field */}
-                {/* <div className='searchDiv'>
-                    <FiSearch className="text-gray-500 group-focus-within:text-darkText duration-200" />
-                    <input type="text" placeholder='search for products'
-                        className='placeholder:text-sm flex-1 outline-none' />
-                </div> */}
-                 {/* Login/Register */}
-                 {!session && (
-                        <div
-                            onClick={() => signIn()}
-                            className='headerDiv'>
-                            <BsCart />
-                            <p className='text-sm font-semibold'>Login/Registar</p>
-                        </div>
-                    )}
-                    {/* Cart button */}
-                    <Link href={'/cart'}>
-                        <div className='cartDiv'>
-                            <BsCart className="text-xl" />
-                            <p className='text-sm font-semibold'>
-                                <FormattedPrice amount={totalAmt ? totalAmt : 0} />
-                            </p>
-                            <span className='cartSpan'>
-                                {productData ? productData?.length : 0}
-                            </span>
-                        </div>
-                    </Link>
-                    {/* {user Image} */}
-                    {
-                        session && <Image
-                            src={session?.user?.image as string}
-                            alt='user image'
-                            width={50}
-                            height={50}
-                            className='rounded-full object-cover' />
-                    }
-                    {/* Order button */}
-                    {
-                        orderData?.order?.length > 0 && session && (
-                            <Link href={"/order"} className='headerDiv px-2 gap-x-1 cursor-pointer'>
-                                <BsBookmark className="text-2xl" />
-                                <p className='text-sm font-semibold'>Orders</p>
-                            </Link>
-                        )
-                    }
+         <div className='bg-bodyColor h-20 top-0 sticky z-50'>
+         <Container className='h-full flex items-center md:gap-x-5 justify-start md:justify-between'>
+             {/* {logo} */}
+             <Logo />
+             {/* Search Field */}
+             {/* <div className='searchDiv'>
+                 <FiSearch className="text-gray-500 group-focus-within:text-darkText duration-200" />
+                 <input type="text" placeholder='search for products'
+                     className='placeholder:text-sm flex-1 outline-none' />
+             </div> */}
+             {/* Login/Register */}
+             {!session && (
+                 <div
+                     onClick={() => signIn()}
+                     className='headerDiv'>
+                     <CiUser />
+                     <p className='text-sm font-semibold'>Login/Registar</p>
+                 </div>
+             )}
+             {/* Cart button */}
+             <Link href={'/cart'}>
+                 <div className='cartDiv'>
+                     <BsCart className="text-xl" />
+                     <p className='text-sm font-semibold'>
+                         <FormattedPrice amount={ totalAmt ? totalAmt : 0} />
+                     </p>
+                     <span className='cartSpan'>
+                         {productData ? productData?.length : 0}
+                     </span>
+                 </div>
+             </Link>
+             {/* {user Image} */}
+             {
+                 session && <Image
+                     src={session?.user?.image as string}
+                     alt='user image'
+                     width={50}
+                     height={50}
+                     className='rounded-full object-cover' />
+             }
+             {/* Order button */}
+             {
+                 orderData?.order?.length > 0 && session && (
+                     <Link href={"/order"} className='headerDiv px-2 gap-x-1 cursor-pointer'>
+                     <BsBookmark className="text-2xl"/>
+                     <p className='text-sm font-semibold'>Orders</p>
+                     </Link>
+                 )
+             }
 
+             {/* logout button */}
+             {session && (
+                 <div
+                     onClick={() => signOut()}
+                     className='headerDiv px-2 gap-x-2 cursor-pointer'>
+                     <FiLogOut className="text-2xl" />
+                     <p className='text-sm font-semibold'>Logout</p>
+                 </div>
+             )}
 
-                    {/* logout button */}
-                    {session && (
-                        <div
-                            onClick={() => signOut()}
-                            className='headerDiv px-2 gap-x-2 cursor-pointer'>
-                            <FiLogOut className="text-2xl" />
-                            <p className='text-sm font-semibold'>Logout</p>
-                        </div>
-                    )}
-
-            </Container>
-        </div>
+         </Container>
+     </div>
     )
 }
 
