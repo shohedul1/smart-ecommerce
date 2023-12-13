9532a0c2e83c06671ca3617c190d92a8ceb761f1
shohidul <shohidul@example.com>
2023-12-13 09:59:05 -0800

    2-commit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Logo = () => {
   return (
     <Link href={"/"}>
-        <h3 className="text-3xl font-semibold hover:text-orange-500 cursor-pointer duration-200">Shopping.</h3>
+        <h3 className="text-3xl font-semibold hover:text-orange-500 cursor-pointer duration-200">LoGo.</h3>
     </Link>
   )
 }
